8db14242c468b79b367698146aa49b3e9115ccc4
Augusto Mota Pinheiro <augustomp55@gmail.com>
2024-02-11 12:05:14 -0500

    feat: removed image saving and added subplots for edge detection results

--- edge.py
+++ edge.py
@@ -4,19 +4,6 @@ from skimage import io
 from utils import gaussian_kernel, filter2d, partial_x, partial_y
 
 
-def show_img(img):
-    plt.imshow(1 - img, cmap='Greys')
-
-    plt.show()
-
-
-def save_img(img, name: str, is_float: bool = True):
-    if (is_float):
-        img = (img * 255).astype('uint8')
-
-    io.imsave(name, img)
-
-
 def gradient_magnitude(x_derived_img, y_derived_img):
     if (x_derived_img.shape != y_derived_img.shape):
         return
@@ -47,15 +34,14 @@ def main():
     magnitude_img = gradient_magnitude(x_derived_img, y_derived_img)
 
     # Visualize results
-    show_img(smoothed_img)
-    show_img(x_derived_img)
-    show_img(y_derived_img)
-    show_img(magnitude_img)
-
-    # Save results
-    # save_img(x_derived_img, image_name + '_x_derived.png')
-    # save_img(y_derived_img, image_name + '_y_derived.png')
-    # save_img(magnitude_img, image_name + '_magnitude_derived.png')
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    ax1.imshow(x_derived_img, cmap='gray')
+    ax1.set_title('X derived image')
+    ax2.imshow(y_derived_img, cmap='gray')
+    ax2.set_title('Y derived image')
+    ax3.imshow(magnitude_img, cmap='gray')
+    ax3.set_title('Gradient magnitude')
+    plt.show()
 
     # END YOUR CODE
 
